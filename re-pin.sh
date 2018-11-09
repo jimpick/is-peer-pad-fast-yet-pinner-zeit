eac70af0c9d4d54d914dc88962b5c2193d1bd0be
Jim Pick <jim@backblend.com>
2018-11-09 12:04:21 -0800

    Backup to git

--- re-pin.sh
+++ re-pin.sh
@@ -1,12 +1,14 @@
 #! /bin/bash
 
-SLEEP=300
+SLEEP=60
 while true; do
   for f in `ls head-cid.*.txt`; do
     CID=$(cat $f)
     echo "$f" $CID
     ipfs pin add -r $CID
   done
+  cp head-cid.*.txt backup
+  (cd backup; git add .; git commit -m backup)
   echo Sleeping for $SLEEP seconds
   sleep $SLEEP
 done
